aed02a9d6601dff909727521f48bf2dd3d0c3d11
winnifrede.wang <winnifrede.wang@vipshop.com>
2023-06-08 17:27:22 +0800

    [plcs#00000]
    ……………………….

--- src/layout/components/header/HeaderOptions.vue
+++ src/layout/components/header/HeaderOptions.vue
@@ -7,15 +7,21 @@
           <el-dropdown-item>Action 1</el-dropdown-item>
           <el-dropdown-item>Action 2</el-dropdown-item>
           <el-dropdown-item>Action 3</el-dropdown-item>
-          <el-dropdown-item divided>退出登录</el-dropdown-item>
+          <el-dropdown-item divided @click="logout()">退出登录</el-dropdown-item>
         </el-dropdown-menu>
       </template>
     </el-dropdown>
   </div>
 </template>
-<script setup lang="ts">
+<script setup>
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+const router = useRouter()
+
 const circleUrl = ref('https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png')
+const logout = () => {
+  router.push('/login')
+}
 </script>
 <style lang="scss" scoped>
 .header-operations {
